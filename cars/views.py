6e827ac2fc5eba0c38ed32c2125b5f17c2a7e650
Jiang <jiangyue0380@gmail.com>
2022-08-20 22:53:59 +1000

    updated project URLs + created first view function + created cars app urls.py

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+# decorator is going to assign certain permission to the function below
 
-# Create your views here.
+
+@api_view(['GET'])
+def cars_list(request):
+    return Response('ok!')
