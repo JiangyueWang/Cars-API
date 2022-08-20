1befe39f97c8b129f4f2b76363c2044518d320ac
Jiang <jiangyue0380@gmail.com>
2022-08-20 23:38:52 +1000

    updated serializer.py + updated cars_list function in view.py

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from cars.models import Car
+from cars.serializers import CarSerializer
 # decorator is going to assign certain permission to the function below
 
 
 @api_view(['GET'])
 def cars_list(request):
-    return Response('ok!')
+    cars = Car.objects.all()
+    serializers = CarSerializer(cars, many=True)
+    return Response(serializers.data)
