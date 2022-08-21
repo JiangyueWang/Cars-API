dafbc87c8893a5400f65389046801cb0ef384282
Jiang <jiangyue0380@gmail.com>
2022-08-21 12:24:48 +1000

    regitsered car model with admin site + compelted post functionality for car + updated RESTframework decimal settings

--- cars/views.py
+++ cars/views.py
@@ -1,12 +1,20 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from cars.models import Car
 from cars.serializers import CarSerializer
 # decorator is going to assign certain permission to the function below
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def cars_list(request):
-    cars = Car.objects.all()
-    serializers = CarSerializer(cars, many=True)
-    return Response(serializers.data)
+    if request.method == 'GET':
+        cars = Car.objects.all()
+        serializers = CarSerializer(cars, many=True)
+        return Response(serializers.data)
+
+    elif request.method == 'POST':
+        serializers = CarSerializer(data=request.data)
+        serializers.is_valid(raise_exception=True)
+        serializers.save()
+        return Response(serializers.data, status=status.HTTP_201_CREATED)
