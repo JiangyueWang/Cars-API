4c62d63c5624cbe0d3a1fafad41367b2108deebd
Jiang <jiangyue0380@gmail.com>
2022-08-24 23:21:57 +1000

    add forignkey to the car model in cars app

--- cars/models.py
+++ cars/models.py
@@ -1,5 +1,7 @@
+from ast import Del
 from django.db import models
-
+import dealerships
+from dealerships.models import Dealership
 # Create your models here.
 
 
@@ -8,3 +10,7 @@ class Car(models.Model):
     model = models.CharField(max_length=255)
     year = models.IntegerField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
+    dealership = models.ForeignKey(Dealership, on_delete=model.CASCADE) 
+    # CASCADE on_delete, every time we delete a dealership from a dealerships table, it will also delete all the cars in the cars table that assocaited with that dealership
+
+    
